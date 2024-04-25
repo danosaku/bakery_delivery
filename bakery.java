61319d25bf1ff2a3f49018885786225f98cec156
Daniel Osaku <danosaku@hotmail.com>
2024-04-25 15:04:44 -0300

    Create bakery.java

---
+++ bakery.java
@@ -0,0 +1,28 @@
+/res
+  /layout
+    activity_login.xml
+    activity_registration.xml
+    activity_bakery_list.xml
+    activity_product_list.xml
+    activity_reservation.xml
+    activity_reservation_history.xml
+/src
+  /java
+    /com/example/bakeryreservation
+      model
+        Bakery.java
+        Product.java
+        Reservation.java
+      adapter
+        BakeryListAdapter.java
+        ProductListAdapter.java
+        ReservationHistoryAdapter.java
+      util
+        DateTimeUtils.java
+      activity
+        LoginActivity.java
+        RegistrationActivity.java
+        BakeryListActivity.java
+        ProductListActivity.java
+        ReservationActivity.java
+        ReservationHistoryActivity.java
